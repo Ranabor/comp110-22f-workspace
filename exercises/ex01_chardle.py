e381b95d513bb83c733929c0162b8edd52f726d8
RishiReddyR <ranabor@unc.edu>
2022-08-21 13:08:47 -0400

    Fixed variable typing and syntax

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,25 +2,20 @@
 
 __author__ = "730575704"
 
-from re import X
-
-
-print("Enter a 5 character word:", end=" ")
-word_choice = input()
+word_choice: str = input("Enter a 5 character word: ")
 if len(word_choice) != 5:
-        print ("Error: Word must contain 5 characters")
-        exit()
+    print("Error: Word must contain 5 characters")
+    exit()
 
-print("Enter a single character:", end=" ")
-char_choice = input()
+char_choice: str = input("Enter a single character: ")
 if len(char_choice) != 1:
-        print ("Error: Character must be a single character")
-        exit()
+    print("Error: Character must be a single character")
+    exit()
 
 print("Searching for", char_choice, "in", word_choice)
 
 
-char_count = 0
+char_count: int = 0
 
 if word_choice[0] == char_choice:
     print(char_choice, "found at index 0")
@@ -44,13 +39,11 @@ if word_choice[4] == char_choice:
 
 if char_count > 0:
     if char_count == 1:
-        print (char_count, "instance of", char_choice, "found in", word_choice)
+        print(char_count, "instance of", char_choice, "found in", word_choice)
     if char_count > 1:
-        print (char_count, "instances of", char_choice, "found in", word_choice)
+        print(char_count, "instances of", char_choice, "found in", word_choice)
 else:
-    print ("No instances of", char_choice, "found in", word_choice)
-
-
+    print("No instances of", char_choice, "found in", word_choice)
 
 """What I originally did before I read 'no loops'"""
 # index_count = 0
